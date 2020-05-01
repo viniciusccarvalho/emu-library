14d7150cd6945c9ec3199e2d117754e24246ba27
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2020-04-30 21:47:02 -0400

    Adding logging support

--- src/main/kotlin/io/igx/emu/config/Modules.kt
+++ src/main/kotlin/io/igx/emu/config/Modules.kt
@@ -2,6 +2,7 @@ package io.igx.emu.config
 
 import io.igx.emu.controllers.DatabaseController
 import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.h2.H2DatabasePlugin
 import org.koin.dsl.module
 
 /**
@@ -18,5 +19,7 @@ val common = module {
 }
 
 fun jdbi() : Jdbi {
-    return Jdbi.create("jdbc:h2:file:~/.emulib/db")
+    val jdbi = Jdbi.create("jdbc:h2:file:~/.emulib/db")
+    jdbi.installPlugin(H2DatabasePlugin())
+    return jdbi
 }
